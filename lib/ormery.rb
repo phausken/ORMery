64ef3183ed13962556f62c1d7a66401ec5fbb6e9
Peter Hausken <phausken@gmail.com>
2017-09-05 16:43:32 -0400

    Add AssocOptions class

--- lib/ormery.rb
+++ lib/ormery.rb
@@ -145,3 +145,19 @@ module Searchable
     parse_all(result)
   end
 end
+
+class AssocOptions
+  attr_accessor(
+    :foreign_key,
+    :class_name,
+    :primary_key
+  )
+
+  def model_class
+    @class_name.constantize
+  end
+
+  def table_name
+    model_class.table_name
+  end
+end
