7bad453542de30dd76998845aa54d97e648624e2
Peter Hausken <phausken@gmail.com>
2017-09-05 16:46:20 -0400

    Add Associatable module

--- lib/ormery.rb
+++ lib/ormery.rb
@@ -4,6 +4,8 @@ require 'active_support/inflector'
 class SQLObject
   extend Searchable
   include Searchable
+  include Associatable
+  extend Associatable
 
   def self.columns
     if @everything.nil?
@@ -193,5 +195,59 @@ class HasManyOptions < AssocOptions
         self.instance_variable_set("@#{k}", v)
       end
 
+  end
+end
+
+
+module Associatable
+  def belongs_to(name, options = {})
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      target_key = self.send(options.foreign_key)
+        options.model_class.where(options.primary_key => target_key)[0]
+
+    end
+  end
+
+  def has_many(name, options = {})
+      self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+      define_method(name) do
+        options = self.class.assoc_options[name]
+        target_key = self.send(options.primary_key)
+          options.model_class.where(options.foreign_key => target_key)
+
+      end
+  end
+
+  def assoc_options
+    @assoc_options ||= {}
+    @assoc_options
+  end
+
+  def has_one_through(name, through_name, source_name)
+    define_method(name) do
+      through = self.class.assoc_options[through_name]
+      source = through.model_class.assoc_options[source_name]
+
+      target_value = self.send(through.foreign_key)
+
+      results = DBConnection.execute(<<-SQL, target_value)
+        SELECT
+          #{source.table_name}.*
+        FROM
+          #{through.table_name}
+        JOIN
+          #{source.table_name}
+        ON
+          #{through.table_name}.#{source.foreign_key} = #{source.table_name}.#{source.primary_key}
+        WHERE
+          #{through.table_name}.#{through.primary_key} = ?
+        SQL
+
+        source.model_class.parse_all(results)[0]
+      end
   end
 end
